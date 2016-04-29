58a3d892c9f78cc2ed8b020976f8be0926b8232e
Desmond Liang <liangyufeng@live.com>
2016-04-28 21:04:15 -0400

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
  * @copyright Copyright (c) 2016, Desmond Liang
  * @version Check ds_core.php
  * @link http://despot.desliang.com
- * @license Open source software: free uses
+ * @license GNU GENERAL PUBLIC LICENSE (see LICENSE.txt)
  * ---------------------------------------------------------
  * This page provides the home page of DeSpot as well as 
  * doing redirections based on the incmoing url.
@@ -22,18 +22,11 @@
 <center>
 
 <?php
-define(PRINT_FORMAT, "homepage");
-
-// Trim leading slash(es)
-$path = ltrim($_SERVER['REQUEST_URI'], '/'); 
-if(!$path) $path = "index";
-
-// Display home page
-$printFormat = "homepage";
-require('ds_header.php');
-$_SESSION["redirect"] = NULL;
-include('ds_posts.php');
-include('ds_footer.php');
+  define(PRINT_FORMAT, "homepage");
+  require('ds_header.php');
+  $_SESSION["redirect"] = NULL;
+  include('ds_posts.php');
+  include('ds_footer.php');
 ?>
 
 </center>
